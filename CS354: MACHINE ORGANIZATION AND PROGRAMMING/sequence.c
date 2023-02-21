6a591cf2bda7e3cbc1b25a92b24f102ede4589e9
2088studios <121307156+2088studios@users.noreply.github.com>
2023-02-21 11:29:58 -0600

    Update sequence.c

--- CS354: MACHINE ORGANIZATION AND PROGRAMMING/sequence.c
+++ CS354: MACHINE ORGANIZATION AND PROGRAMMING/sequence.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#Sequence where next value in sequence is computed as x_n = m*x_0 + b
-
+/*Sequence where next value in sequence is computed as x_n = m*x_0 + b*/
 int main(int argc, char **argv)
 {
 
@@ -12,7 +11,7 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-#non-zero positive number of values in the sequence
+/*non-zero positive number of values in the sequence*/
 int n = atoi(argv[1]);
     
 if (n <= 0)
@@ -20,13 +19,13 @@ if (n <= 0)
         exit(1);
     }
     
-#an integer and is the first value in the sequence
+/*an integer and is the first value in the sequence*/
 int x = atoi(argv[2]);
 
-#integer and is used as a multiplier of the previous term in the sequence
+/*integer and is used as a multiplier of the previous term in the sequence*/
 int m = atoi(argv[3]);
 
-#an integer and is added to the (m*previous) term
+/*an integer and is added to the (m*previous) term*/
 int b = atoi(argv[4]);
 
     printf("%i", x);
